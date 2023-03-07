8d24c092a20a139ab944b07783157ac7972ce440
RudolfJB <you.invention@gmail.com>
2023-03-07 11:31:27 +0200

    my OOP

--- hello.py
+++ hello.py
@@ -1,12 +1,28 @@
 print("hello world")
 
 
-
-
 def hello():
     print("love you lots")
 
 
+hello()
 
 
-hello() 
+# my first OOP
+
+
+class my_OOP():
+    def __init__(self, **kw):
+        super(my_OOP, self).__init__(**kw)
+
+    def print_this(self, val):
+        print(val)
+
+    def main(self):
+        val = input(">>")
+        self.print_this(val)
+
+
+my = my_OOP()
+
+my.main()
