0b9d632d043344c898393f1f714007a2d8f3ce25
RudolfJB <you.invention@gmail.com>
2023-03-07 11:11:43 +0200

    problem set 0 einstein

---
+++ problem_set_0/einstein.py
@@ -0,0 +1,21 @@
+def main():
+
+    # Ask user to input a mass in kilograms
+    entry = input("Please enter any number of mass in kilograms ")
+
+    # Apply einstein's famous E=mc*2
+    calc = int.__pow__(3000000000, 2)
+    E = calc * int(entry)
+    print("m:", entry)
+    print("E:", E)
+
+
+def calc(c):
+    return c * c
+
+
+def formula(e):
+    return (e)
+
+
+main()
